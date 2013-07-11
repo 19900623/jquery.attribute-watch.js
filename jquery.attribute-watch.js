fda877819c73bd9ce62641c421a1294213c062e0
Ernesto <delrosario.ernesto@gmail.com>
2013-07-12 01:54:33 +0300

    Unwatch method implemented

--- jquery.attribute-watch.js
+++ jquery.attribute-watch.js
@@ -23,9 +23,10 @@ jQuery.fn.watch = function (props, callback, timeout) {
 		el.data(data);
 
 		if (( typeof (this.onpropertychange) == 'object') || ($.browser.mozilla)) {
-			el.bind('DOMAttrModified', callback);
+			el.on('DOMAttrModified', callback);
 		} else {
-			setInterval(func, timeout);
+			timer = setInterval(func, timeout);
+			el.data('timer', timer);
 		}
 	});
 	
@@ -47,4 +48,34 @@ jQuery.fn.watch = function (props, callback, timeout) {
 			data.func.call(el, data);
 		}
 	}
+}
+
+jQuery.fn.unwatch = function (props) {
+	var $ = jQuery;
+	
+	return this.each(function () {
+		var el = $(this),
+			data = el.data(),
+			timer = el.data('timer');
+		
+		props = props.split(','); 
+		
+		if (props.length) {
+			$.each(data.props, function (i) {
+				if ($.inArray(data.props[i], props) > -1) {
+					data.props.splice(i, 1);
+					data.vals.splice(i, 1);
+				}
+			});
+		} else {
+			el.removeData(['props', 'vals']);
+		}
+
+		if (( typeof (this.onpropertychange) == 'object') || ($.browser.mozilla)) {
+			el.off('DOMAttrModified');
+		} else {
+			clearInterval(timer);
+			el.removeData('timer');
+		}
+	});
 }
